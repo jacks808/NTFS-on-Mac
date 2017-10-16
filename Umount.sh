6c0aeab38865f063dfc9531ed5756cfd3d633cf8
ziyi.wang <jacks808@163.com>
2017-10-15 22:28:04 -0500

    Create Umount.sh
    Add umount.sh

---
+++ Umount.sh
@@ -0,0 +1,6 @@
+echo "###########################################"
+echo "#          开始卸载USB NTFS硬盘           #"
+echo "#     Begin to umount USB NTFS Disk       #"
+echo "###########################################"
+
+sudo umount ~/Desktop/Disk/
